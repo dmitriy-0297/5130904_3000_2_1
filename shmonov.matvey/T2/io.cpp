f00eb4e316070525970544e6661f65c19912b72d
Matvey Shmonov <shmonov.mv@edu.spbstu.ru>
2024-03-26 22:29:28 +0300

    Initial commit for T2

---
+++ shmonov.matvey/T2/io.cpp
@@ -0,0 +1,86 @@
+#include <string>
+
+#include "io.h"
+
+std::istream& shmonov::operator>>(std::istream& in, shmonov::DataStruct& ds) {
+  std::istream::sentry sentry(in);
+  if (!sentry) {
+    return in;
+  }
+
+  std::string str;
+  std::getline(in, str);
+  std::istringstream iss(str.substr(1, str.size() - 2));
+  std::string token;
+  ds.valid = true;
+
+  while (std::getline(iss, token, ':')) {
+    if (!token.empty()) {
+      std::string key = token.substr(0, 4);
+      std::string value = token.substr(5);
+      if (key == "key1") {
+        if (!shmonov::isDBL_SCI(value)) {
+          ds.valid = false;
+          break;
+        }
+        ds.key1 = std::stod(value);
+      }
+      else if (key == "key2") {
+        if (!shmonov::isChar(value)) {
+          ds.valid = false;
+          break;
+        }
+        ds.key2 = value[1];
+      }
+      else if (key == "key3") {
+        if (!shmonov::isString(value)) {
+          ds.valid = false;
+          break;
+        }
+        ds.key3 = value.substr(1, value.size() - 2);
+      }
+    }
+  }
+  return in;
+}
+
+std::ostream& shmonov::operator<<(std::ostream& out, const shmonov::DataStruct& ds) {
+  std::ostream::sentry sentry(out);
+  if (!sentry) {
+    return out;
+  }
+  iofmtguard fmtguard(out);
+  out << "(:key1 " << std::setprecision(2) << std::fixed << std::scientific << ds.key1 << ":";
+  out << "key2 '" << ds.key2 << "':";
+  out << "key3 \"" << ds.key3 << "\":)";
+  return out;
+}
+
+bool shmonov::isDBL_SCI(const std::string& str) {
+  std::regex pattern("^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$");
+  return std::regex_match(str, pattern);
+}
+
+bool shmonov::isChar(const std::string& str) {
+  std::regex pattern("'.'");
+  return std::regex_match(str, pattern);
+}
+
+bool shmonov::isString(const std::string& str) {
+  std::regex pattern("\".*\"");
+  return std::regex_match(str, pattern);
+}
+
+shmonov::iofmtguard::iofmtguard(std::basic_ios< char >& s) :
+  s_(s),
+  fill_(s.fill()),
+  precision_(s.precision()),
+  fmt_(s.flags())
+{}
+
+shmonov::iofmtguard::~iofmtguard()
+{
+  s_.fill(fill_);
+  s_.precision(precision_);
+  s_.flags(fmt_);
+}
---
+++ shmonov.matvey/T2/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+#include "io.h"
+
+using namespace shmonov;
+
+int main( void )  // (:key1 5.45e-2:key2 'c':key3 "Hello world!")
+{
+  std::vector<DataStruct> vec;
+  std::copy_if(std::istream_iterator<DataStruct>(std::cin),
+      std::istream_iterator<DataStruct>(),
+      std::back_inserter(vec),
+      [](const DataStruct &ds) { return ds.valid; });
+    
+  std::sort(vec.begin(), vec.end(),
+    [](const DataStruct &lhs, const DataStruct &rhs) {
+      if (lhs.key1 < rhs.key1) {
+        return true;
+      }
+      if (lhs.key2 < rhs.key2) {
+        return true;
+      }
+      else {
+        return lhs.key3 < rhs.key3;
+      }
+    }
+  );
+  std::copy(vec.begin(), vec.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
+}
