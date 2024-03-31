781c6b1e4f7f1e1fe0baaf210659c0bc2dd46e04
Reivis0 <anna.sevvost@gmail.com>
2024-04-01 00:21:22 +0300

    correct whitespace in T2

--- sevostyanova.anna/T2/io.h
+++ sevostyanova.anna/T2/io.h
@@ -11,21 +11,20 @@
 
 namespace sevostyanova
 {
-	struct Data
-	{
-		unsigned long long key1_;
-		unsigned long long key2_;
-		std::string key3_;
-		friend std::istringstream& operator>>(std::istringstream& input, Data& ds);
-		friend std::ostream& operator<<(std::ostream& out, const Data& ds);
-		
-	};
+  struct Data
+  {
+    unsigned long long key1_;
+    unsigned long long key2_;
+    std::string key3_;
+    friend std::istringstream& operator>>(std::istringstream& input, Data& ds);
+    friend std::ostream& operator<<(std::ostream& out, const Data& ds);		
+  };
 
-	std::pair<std::string, std::string> getPair(std::string& str);
-	bool isULLLIFT(const std::string& str);
-	bool isULLBIN(const std::string& str);
-	bool isString(const std::string& str);
-	bool isCorrectTypes(std::string& input);
+  std::pair<std::string, std::string> getPair(std::string& str);
+  bool isULLLIFT(const std::string& str);
+  bool isULLBIN(const std::string& str);
+  bool isString(const std::string& str);
+  bool isCorrectTypes(std::string& input);
 
 }
 #endif
