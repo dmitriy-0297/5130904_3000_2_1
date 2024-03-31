74da7221a7e89951c4b350d2da72fc8aa1b59a91
B0kyto <konstantinsimonov61@gmail.com>
2024-03-31 22:13:44 +0300

    remove all tabs in Io.h

--- simonov.konstantin/T2/Io.h
+++ simonov.konstantin/T2/Io.h
@@ -7,58 +7,58 @@
 
 namespace simonov
 {
-	struct Data
-	{
-		unsigned long long key1;
-		unsigned long long key2;
-		std::string key3;
-		bool thatIsHorror = false;
-	};
+  struct Data
+  {
+    unsigned long long key1;
+    unsigned long long key2;
+    std::string key3;
+    bool thatIsHorror = false;
+  };
 
-	struct ULLLit
-	{
-		unsigned long long& valueLit;
-	};
+  struct ULLLit
+  {
+    unsigned long long& valueLit;
+  };
 
-	struct ULLOct
-	{
-		unsigned long long& valueOct;
-	};
+  struct ULLOct
+  {
+    unsigned long long& valueOct;
+  };
 
-	struct StrKey
-	{
-		std::string& valueStr;
-	};
+  struct StrKey
+  {
+    std::string& valueStr;
+  };
 
-	struct Border
-	{
-		std::vector<std::string> borders;
-		short length_;
-		Border(const std::vector<std::string> myVector, const short len) :
-			borders(myVector), length_(len)
-		{
-		}
-	};
+  struct Border
+  {
+    std::vector<std::string> borders;
+    short length_;
+    Border(const std::vector<std::string> myVector, const short len) :
+      borders(myVector), length_(len)
+    {
+    }
+  };
 
-	class StreamGuard
-	{
-	public:
-		StreamGuard(std::basic_ios< char >& s);
-		~StreamGuard();
-	private:
-		std::basic_ios< char >& strm_;
-		char fill_;
-		std::streamsize precision_;
-		std::basic_ios< char >::fmtflags fmt_;
-	};
+  class StreamGuard
+  {
+  public:
+    StreamGuard(std::basic_ios< char >& s);
+    ~StreamGuard();
+  private:
+    std::basic_ios< char >& strm_;
+    char fill_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags fmt_;
+  };
 
-	std::istream& operator >> (std::istream& in, Border&& fence);
-	std::istream& operator >> (std::istream& in, StrKey&& str);
-	std::istream& operator >> (std::istream& in, ULLLit&& lit);
-	std::istream& operator >> (std::istream& in, ULLOct&& oct);
-	std::istream& operator >> (std::istream& in, Data& data);
-	std::ostream& operator << (std::ostream& out, const Data& fence);
-	void whichKeyYouWrote(const std::string& key, Data& data, std::istream& stream, bool flags[]);
+  std::istream& operator >> (std::istream& in, Border&& fence);
+  std::istream& operator >> (std::istream& in, StrKey&& str);
+  std::istream& operator >> (std::istream& in, ULLLit&& lit);
+  std::istream& operator >> (std::istream& in, ULLOct&& oct);
+  std::istream& operator >> (std::istream& in, Data& data);
+  std::ostream& operator << (std::ostream& out, const Data& fence);
+  void whichKeyYouWrote(const std::string& key, Data& data, std::istream& stream, bool flags[]);
 }
 
 #endif // !IO_H
