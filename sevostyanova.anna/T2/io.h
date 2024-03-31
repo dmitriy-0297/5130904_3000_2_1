69d96ab90d6539550a1b3c49cc989c01a77a4bec
Reivis0 <anna.sevvost@gmail.com>
2024-03-31 23:14:07 +0300

    initial commit for T2

---
+++ sevostyanova.anna/T2/io.h
@@ -0,0 +1,31 @@
+#ifndef IO_H
+#define IO_H
+
+#include <iostream>
+#include<string>
+#include <sstream>
+#include<algorithm>
+#include<regex>
+#include<bitset>
+
+
+namespace sevostyanova
+{
+	struct Data
+	{
+		unsigned long long key1_;
+		unsigned long long key2_;
+		std::string key3_;
+		friend std::istringstream& operator>>(std::istringstream& input, Data& ds);
+		friend std::ostream& operator<<(std::ostream& out, const Data& ds);
+		
+	};
+
+	std::pair<std::string, std::string> getPair(std::string& str);
+	bool isULLLIFT(const std::string& str);
+	bool isULLBIN(const std::string& str);
+	bool isString(const std::string& str);
+	bool isCorrectTypes(std::string& input);
+
+}
+#endif
---
+++ sevostyanova.anna/T2/main.cpp
@@ -0,0 +1,36 @@
+﻿#include"io.h"
+#include<vector>
+
+
+int main()
+{
+	std::vector<sevostyanova::Data> vc;
+	std::string str = "";
+	while (!std::cin.eof())
+	{
+		std::getline(std::cin, str,'\n');
+		if (str == "" || std::cin.eof())
+			break;
+		if (sevostyanova::isCorrectTypes(str))
+		{
+			std::istringstream input(str);
+			sevostyanova::Data ds;
+			input >> ds;
+			vc.push_back(ds);
+		}
+	}
+	std::sort(vc.begin(), vc.end(),
+		[](const sevostyanova::Data& el1, const sevostyanova::Data& el2)
+		{
+			if (el1.key1_ == el2.key1_)
+			{
+				if (el1.key2_ == el2.key2_)
+					return el1.key3_.length() < el2.key3_.length();
+				return el1.key2_ < el2.key2_;
+			}
+			return el1.key1_ < el2.key1_;
+		});
+	std::copy(vc.begin(), vc.end(), std::ostream_iterator<sevostyanova::Data>(std::cout));
+
+	return EXIT_SUCCESS;
+}
