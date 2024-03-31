8f8830bcd0472b9df234aa88d0a5bacac47951da
Vladislav Zlatov <163848800+wywyy3cee@users.noreply.github.com>
2024-03-31 21:44:10 +0300

    Update main.cpp for new files

--- zlatov.vladislav/T2/main.cpp
+++ zlatov.vladislav/T2/main.cpp
@@ -1,7 +1,23 @@
-﻿#include "io.h"
-#include <iostream>
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "DataStruct.h"
+#include "Delimiter.h"
+#include "ScopeGuard.h"
 
 int main() {
-  zlatov::processData();
+  std::vector<DataStruct> data;
+  DataStruct temp;
+
+  while (std::cin >> temp) {
+    data.push_back(temp);
+  }
+
+  std::sort(data.begin(), data.end());
+
+  for (const auto& item : data) {
+    std::cout << item << '\n';
+  }
+
   return 0;
 }
