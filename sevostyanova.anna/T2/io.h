6d7d5254b272833adb53713a7cf8509ed575773a
Reivis0 <anna.sevvost@gmail.com>
2024-04-02 20:57:18 +0300

    correct mistakes with namespace v2

--- sevostyanova.anna/T2/io.h
+++ sevostyanova.anna/T2/io.h
@@ -15,13 +15,14 @@ namespace sevostyanova
     unsigned long long key1_;
     unsigned long long key2_;
     std::string key3_;
-    friend std::istringstream & operator >>(std::istringstream &input, Data &ds);
-    friend std::ostream & operator <<(std::ostream &out, const Data &ds);
   };
   std::pair<std::string, std::string> getPair(std::string &str);
   bool isULLLIFT(const std::string &str);
   bool isULLBIN(const std::string &str);
   bool isString(const std::string &str);
   bool isCorrectTypes(std::string &input);
+
+  std::istringstream& operator >>(std::istringstream& input, Data& ds);
+  std::ostream& operator <<(std::ostream& out, const Data& ds);
 }
 #endif
