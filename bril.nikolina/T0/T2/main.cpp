a2ac88b1c3fb0cbf450ded16a8b0ebefd41bcbce
brilnn <148279337+brilnn@users.noreply.github.com>
2024-04-03 02:05:14 +0300

    delete one more time

--- bril.nikolina/T0/T2/main.cpp
+++ bril.nikolina/T0/T2/main.cpp
@@ -7,30 +7,24 @@
 #include <iomanip>
 #include <algorithm>
 
-namespace nspace
-{
+namespace nspace {
     struct DataStruct {
         char key1;
         std::pair<long long, unsigned long long> key2;
         std::string key3;
     };
-    
     struct DelimiterIO
     {
         char exp;
     };
-
     struct Raitional
     {
         std::pair<long long, unsigned long long>& ref; 
     };
-
-
     struct StringIO
     {
         std::string& ref;
     };
-
     class iofmtguard {
     public:
         iofmtguard(std::basic_ios< char >& s);
@@ -41,7 +35,6 @@ namespace nspace
         std::streamsize precision_;
         std::basic_ios< char >::fmtflags fmt_;
     };
-
     std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
     std::istream& operator>>(std::istream& in, Raitional&& dest);
     std::istream& operator>>(std::istream& in, StringIO&& dest);
