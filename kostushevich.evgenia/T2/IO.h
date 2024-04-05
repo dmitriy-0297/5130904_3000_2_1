71598f329a6cce6d8d753ba7d420da19bf661422
kostushh <kostushevichevgen@yandex.ru>
2024-04-06 02:59:12 +0300

    Initial commit for T2

---
+++ kostushevich.evgenia/T2/IO.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <iostream>
+#include <iterator>
+#include <complex>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <iomanip>
+
+namespace kostushevich
+{
+    struct DataStruct
+    {
+        unsigned long long key1;
+        std::complex<double> key2;
+        std::string key3;
+    };
+    struct DelimiterIO
+    {
+        char exp;
+    };
+    struct OctIO
+    {
+        unsigned long long& ref;
+    };
+    struct ComplexIO
+    {
+        std::complex<double>& ref;
+    };
+    struct StringIO
+    {
+        std::string& ref;
+    };
+    struct LabelIO
+    {
+        std::string exp;
+    };
+
+    class iofmtguard
+    {
+    public:
+        iofmtguard(std::basic_ios< char >& s);
+        ~iofmtguard();
+    private:
+        std::basic_ios< char >& s_;
+        char fill_;
+        std::streamsize precision_;
+        std::basic_ios< char >::fmtflags fmt_;
+    };
+    std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
+    std::istream& operator>>(std::istream& in, OctIO&& dest);
+    std::istream& operator>>(std::istream& in, ComplexIO&& dest);
+    std::istream& operator>>(std::istream& in, StringIO&& dest);
+    std::istream& operator>>(std::istream& in, LabelIO&& dest);
+    std::istream& operator>>(std::istream& in, DataStruct& dest);
+    std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
+}
---
+++ kostushevich.evgenia/T2/main.cpp
@@ -0,0 +1,55 @@
+﻿#include "IO.h"
+
+using namespace kostushevich;
+
+int main()
+{
+    std::vector<DataStruct> data;
+    std::string input = "";
+    while (std::cin.good() && !(std::cin.eof()))
+    {
+        std::getline(std::cin, input);
+        std::istringstream iss(input);
+        if (input.empty())
+        {
+            break;
+        }
+
+        std::copy(
+            std::istream_iterator<DataStruct>(iss),
+            std::istream_iterator<DataStruct>(),
+            std::back_inserter(data)
+        );
+    }
+
+    std::sort(
+        data.begin(),
+        data.end(),
+        [](const DataStruct& a, const DataStruct& b)
+        {
+            if (a.key1 < b.key1)
+            {
+                return true;
+            }
+            if (a.key1 == b.key1)
+            {
+                if (std::abs(a.key2) < std::abs(b.key2))
+                {
+                    return true;
+                }
+                if (std::abs(a.key2) == std::abs(b.key2))
+                {
+                    return a.key3.size() < b.key3.size();
+                }
+            }
+            return false;
+        }
+    );
+
+    std::copy(
+        std::begin(data),
+        std::end(data),
+        std::ostream_iterator<DataStruct>(std::cout, "\n")
+    );
+    return EXIT_SUCCESS;
+}
