31e46fe61166908d21ec3f185ce8b64df89149bf
berdyanskij.roman <mega_roma06@mail.ru>
2024-04-14 17:26:05 +0300

    Initial commit for T2

--- berdyanskij.roman/T2/io.cpp
+++ berdyanskij.roman/T2/io.cpp
@@ -1,40 +1,119 @@
-
-#include <sstream>
-#include <iomanip>
-
 #include "io.h"
 
+std::pair<std::string, std::string> berdyanskij::get_next_pair(std::string& s)
+{
+  s.erase(0, s.find(':') + 1); // remove ':' from the beginning of string
+  std::string key = s.substr(0, 4);
+  s.erase(0, 5);
+  std::string value;
+  if (key == "key3")
+  {
+    value = s.substr(0, s.find('"', 1) + 1);
+    s.erase(0, value.size());
+  }
+  else
+  {
+    value = s.substr(0, s.find(':'));
+    s.erase(0, value.size());
+  }
+  return std::make_pair(key, value); // key, value
+}
+std::istream& berdyanskij::operator>>(std::istream& in, berdyanskij::DataStruct& ds)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
 
-std::istream& berdyanskij::operator>>(std::istream& is, berdyanskij::DataStruct& ds) {
-  std::string line;
-  if (std::getline(is, line, '(') && std::getline(is, line, ')')) {
-    std::istringstream iss(line);
-    std::string part;
-    while (std::getline(iss, part, ':')) {
-      std::string key, value;
-      std::istringstream part_stream(part);
-      if (part_stream >> key >> value) {
-        if (key == "key1") {
-          ds.key1 = std::stod(value);
-        }
-        else if (key == "key2") {
-        size_t end_pos = value.find_first_not_of("0123456789-");
-        if (end_pos != std::string::npos) {
-          value = value.substr(0, end_pos);
-        }
-          ds.key2 = std::stoll(value);
-        }
-        else if (key == "key3") {
-          ds.key3 = value.substr(1, value.length() - 2);
-        }
+  ds.valid = true;
+  std::string token;
+  getline(in, token);
+  while (!token.empty())
+  {
+    std::pair<std::string, std::string> pair = berdyanskij::get_next_pair(token);
+    if (pair.first == "key1")
+    {
+      if (!berdyanskij::is_double(pair.second))
+      {
+        ds.valid = false;
+        break;
       }
+      ds.key1 = std::stod(pair.second);
+    }
+    else if (pair.first == "key2")
+    {
+      if (!berdyanskij::is_long(pair.second)) {
+        ds.valid = false;
+        break;
+      }
+      ds.key2 = std::stoll(pair.second);
+    }
+    else if (pair.first == "key3")
+    {
+      if (!berdyanskij::is_string(pair.second))
+      {
+        ds.valid = false;
+        break;
+      }
+      ds.key3 = pair.second.substr(1, pair.second.size() - 2);
     }
   }
-  return is;
+  return in;
+}
+std::string berdyanskij::get_scientific(double x)
+{
+  std::stringstream ss;
+  ss << std::scientific << x;
+  std::string out = ss.str();
+  size_t i = std::min(out.find('E'), out.find('e'));
+  while (out[i - 1] == '0' && out[i - 2] != '.')
+  {
+    out.erase(i - 1, 1);
+    i = std::min(out.find('E'), out.find('e'));
+  }
+  while (out[i + 2] == '0')
+  {
+    out.erase(i + 2, 1);
+    i = std::min(out.find('E'), out.find('e'));
+  }
+  return out;
+}
+std::ostream& berdyanskij::operator<<(std::ostream& out, const berdyanskij::DataStruct& ds) {
+  std::ostream::sentry sentry(out);
+  if (!sentry)
+  {
+    return out;
+  }
+  iofmtguard fmtguard(out);
+  out << "(:key1 " << get_scientific(ds.key1) << ":key2 " << ds.key2 << ":key3 \"" << ds.key3 << "\":)";
+  return out;
 }
 
-std::ostream& berdyanskij::operator<<(std::ostream& os, const berdyanskij::DataStruct& ds) {
-  os << "(:key1 " << std::scientific << std::nouppercase << ds.key1
-    << ":key2 " << ds.key2 << ":key3 \"" << ds.key3 << "\":)";
-  return os;
+bool berdyanskij::is_double(const std::string& str)
+{
+  std::regex pattern("^[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)$");
+  return std::regex_match(str, pattern);
+}
+bool berdyanskij::is_long(const std::string& str)
+{
+  std::regex pattern("^-?[0-9]+(ll|LL)?$");
+  return std::regex_match(str, pattern);
+}
+bool berdyanskij::is_string(const std::string& str)
+{
+  std::regex pattern("\".*\"");
+  return std::regex_match(str, pattern);
+}
+berdyanskij::iofmtguard::iofmtguard(std::basic_ios< char >& s) :
+  s_(s),
+  fill_(s.fill()),
+  precision_(s.precision()),
+  fmt_(s.flags())
+{}
+berdyanskij::iofmtguard::~iofmtguard()
+{
+  s_.fill(fill_);
+  s_.precision(precision_);
+  s_.flags(fmt_);
 }
--- berdyanskij.roman/T2/main.cpp
+++ berdyanskij.roman/T2/main.cpp
@@ -1,29 +1,35 @@
-#include <vector>
+#include <iostream>
 #include <algorithm>
 #include <iterator>
+#include <vector>
 
 #include "io.h"
 
 using namespace berdyanskij;
 
-int main() {
+int main(void)  // (:key1 5.45e-2:key2 2ll:key3 "Hello world!")
+{
   std::vector<DataStruct> data;
-  std::copy(std::istream_iterator<DataStruct>(std::cin),
+  std::copy_if(
+    std::istream_iterator<DataStruct>(std::cin),
     std::istream_iterator<DataStruct>(),
-    std::back_inserter(data));
-
-  std::sort(data.begin(), data.end(), [](const DataStruct& a, const DataStruct& b) {
-    if (a.key1 != b.key1) return a.key1 < b.key1;
-    if (a.key2 != b.key2) return a.key2 < b.key2;
-    return a.key3.size() < b.key3.size();
-  });
-
+    std::back_inserter(data),
+    [](const DataStruct& ds) { return ds.valid; }
+  );
+  std::sort(
+    data.begin(),
+    data.end(),
+    [](const DataStruct& a, const DataStruct& b)
+    {
+      if (a.key1 != b.key1)
+      {
+        return a.key1 < b.key1;
+      }
+      if (a.key2 != b.key2)
+      {
+        return a.key2 < b.key2;
+      }
+      return a.key3.size() < b.key3.size();
+    });
   std::copy(data.begin(), data.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
-  return 0;
 }
-
-//(:key1 3.14e0:key2 42: key3 "example1":)
-//(:key1 3.14e0:key2 42: key3 "example111":)
-//(:key1 3.14e0:key2 42: key3 "example1":)
-//(:key1 3.14e0:key2 42: key3 "example11":)
-//^Z
