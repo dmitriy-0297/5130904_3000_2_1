4b7cd1f3617934e2852c1e094869dcb1c0d1cd2e
yahwee <163851516+yahwee@users.noreply.github.com>
2024-04-17 07:42:06 +0300

    Update main.cpp

--- panchenko.maria/T2/main.cpp
+++ panchenko.maria/T2/main.cpp
@@ -5,16 +5,17 @@
 
 int main() {
     std::vector<panchenko::DataStruct> dataVector;
+
     panchenko::DataStruct data;
-    
     while ((std::cin >> data) || (!std::cin.eof() && std::cin.fail())) {
         if (std::cin.fail()) {
-            std::cin.clear();
-            continue;
+            std::cin.clear(); 
+            continue; 
         }
         dataVector.push_back(data);
     }
-std::sort(dataVector.begin(), dataVector.end(), panchenko::compareDataStruct);
+
+    std::sort(dataVector.begin(), dataVector.end(), panchenko::compareDataStruct);
 
     for (const auto& data : dataVector) {
         std::cout << data << std::endl;
