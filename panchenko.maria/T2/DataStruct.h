bfb9de9cb21a7b1206def6f7857cadbc34b1bb94
yahwee <163851516+yahwee@users.noreply.github.com>
2024-04-17 06:34:04 +0300

    Create DataStruct.h

---
+++ panchenko.maria/T2/DataStruct.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+namespace panchenko {
+	struct DataStruct
+	{
+		unsigned long long key1;
+		unsigned long long key2;
+		std::string key3;
+
+		DataStruct() : key1(0), key2(0), key3("") {}
+	};
+
+	std::istream& operator>>(std::istream& is, DataStruct& data);
+	bool compareDataStruct(const DataStruct& a, const DataStruct& b);
+	std::ostream& operator<<(std::ostream& os, const DataStruct& data);	
+	int decToBin(int num);
+}
