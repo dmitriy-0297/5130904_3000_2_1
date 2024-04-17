a1eadf27e3720ca7aef27e4b1bce2ad7da731705
yahwee <163851516+yahwee@users.noreply.github.com>
2024-04-17 06:46:16 +0300

    Update DataStruct.h

--- panchenko.maria/T2/DataStruct.h
+++ panchenko.maria/T2/DataStruct.h
@@ -3,15 +3,13 @@
 #include <string>
 
 namespace panchenko {
-	struct DataStruct
+    struct DataStruct
 	{
 		unsigned long long key1;
 		unsigned long long key2;
 		std::string key3;
-
 		DataStruct() : key1(0), key2(0), key3("") {}
 	};
-
 	std::istream& operator>>(std::istream& is, DataStruct& data);
 	bool compareDataStruct(const DataStruct& a, const DataStruct& b);
 	std::ostream& operator<<(std::ostream& os, const DataStruct& data);	
