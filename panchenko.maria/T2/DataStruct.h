3fadc7380d3cb0c4ca1f11b5536ca5f268e35d1d
yahwee <163851516+yahwee@users.noreply.github.com>
2024-04-17 07:26:26 +0300

    Update DataStruct.h

--- panchenko.maria/T2/DataStruct.h
+++ panchenko.maria/T2/DataStruct.h
@@ -5,13 +5,15 @@
 namespace panchenko {
     struct DataStruct
     {
-	    unsigned long long key1;
-		unsigned long long key2;
-		std::string key3;
-		DataStruct() : key1(0), key2(0), key3("") {}
-	};
-	std::istream& operator>>(std::istream& is, DataStruct& data);
-	bool compareDataStruct(const DataStruct& a, const DataStruct& b);
-	std::ostream& operator<<(std::ostream& os, const DataStruct& data);
+        unsigned long long key1;
+        unsigned long long key2;
+        std::string key3;
+
+        DataStruct() : key1(0), key2(0), key3("") {}
+    };
+
+    std::istream& operator>>(std::istream& is, DataStruct& data);
+    bool compareDataStruct(const DataStruct& a, const DataStruct& b);
+    std::ostream& operator<<(std::ostream& os, const DataStruct& data);
     int decToBin(int num);
 }
