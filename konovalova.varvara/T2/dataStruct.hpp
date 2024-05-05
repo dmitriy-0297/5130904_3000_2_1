8df8b67065b8fe9cd306ddbff875aa751a8b93ad
bazber <varvara.konovalova.06@mail.ru>
2024-05-05 18:04:45 +0300

    CG fix

--- konovalova.varvara/T2/dataStruct.hpp
+++ konovalova.varvara/T2/dataStruct.hpp
@@ -7,17 +7,14 @@
 
 namespace konovalova
 {
-    struct DataStruct
-    {
-        unsigned long long key1;
-        std::complex<double> key2;
-        std::string key3;
-
-        bool operator<(const DataStruct & other) const noexcept;
-    };
-
-    std::istream & operator>>(std::istream & in, DataStruct & data);
-    std::ostream & operator<<(std::ostream & out, const DataStruct & data);
+  struct DataStruct
+  {
+    unsigned long long key1;
+    std::complex<double> key2;
+    std::string key3;
+    bool operator<(const DataStruct& other) const noexcept;
+  };
+  std::istream& operator>>(std::istream& in, DataStruct& data);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& data);
 }
-
 #endif
