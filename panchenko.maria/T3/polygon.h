60ac8a7e37f17013939a3f4820eba99bf4564c7d
yahwee <163851516+yahwee@users.noreply.github.com>
2024-05-28 19:33:53 +0300

    Update polygon.h: fixed indentation

--- panchenko.maria/T3/polygon.h
+++ panchenko.maria/T3/polygon.h
@@ -5,23 +5,23 @@
 
 namespace panchenko {
     struct Point
-	{
-		int x, y;
+    {
+        int x, y;
 
-	};
+    };
 
-	struct Polygon
-	{
-		std::vector<Point> points;
-	};
+    struct Polygon
+    {
+        std::vector<Point> points;
+    };
 
-	std::istream& operator>>(std::istream& input, Point& point);
-	std::ostream& operator<<(std::ostream& output, const Point& point);
-	bool comparePoints(const Point&, const Point&);
+    std::istream& operator>>(std::istream& input, Point& point);
+    std::ostream& operator<<(std::ostream& output, const Point& point);
+    bool comparePoints(const Point&, const Point&);
 
-	std::istream& operator>>(std::istream& input, Polygon& polygon);
-	std::ostream& operator<<(std::ostream& output, const Polygon& polygon);
-	double getArea(const Polygon& polygon);
+    std::istream& operator>>(std::istream& input, Polygon& polygon);
+    std::ostream& operator<<(std::ostream& output, const Polygon& polygon);
+    double getArea(const Polygon& polygon);
 }
 
 #endif
