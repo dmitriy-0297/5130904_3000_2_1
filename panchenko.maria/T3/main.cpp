c6fe4ab01c6254d30b892f0cb38b5f620d761063
yahwee <163851516+yahwee@users.noreply.github.com>
2024-05-28 23:21:28 +0300

    Update main.cpp

--- panchenko.maria/T3/main.cpp
+++ panchenko.maria/T3/main.cpp
@@ -73,7 +73,7 @@ int main(int argc, char* argv[]) {
                 panchenko::same(std::cin, std::cout, polygons);
             }
             else {
-                std::cout << "    <INVALID COMMAND>" << std::endl;
+                std::cout << "<INVALID COMMAND>" << std::endl;
             }
         }
     }
