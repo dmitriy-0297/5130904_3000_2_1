cda9a83bf907122c5dad061aeb149b9485f441c2
yahwee <163851516+yahwee@users.noreply.github.com>
2024-05-28 21:11:49 +0300

    Update commands.h

--- panchenko.maria/T3/commands.h
+++ panchenko.maria/T3/commands.h
@@ -10,12 +10,12 @@
 const std::string ERROR_OF_COMMAND_MSG = "<INVALID COMMAND>";
 
 namespace panchenko {
-	void area(const std::vector<panchenko::Polygon>& polygons, const std::string& param);
-	void min(const std::vector<panchenko::Polygon>& polygons);
-	void max(const std::vector<panchenko::Polygon>& polygons);
-	void count(const std::vector<panchenko::Polygon>& polygons, const std::string& param);
-	void lessArea(const std::vector<panchenko::Polygon>& polygons, const panchenko::Polygon& polygon);
-	void same(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons);
+    void area(const std::vector<panchenko::Polygon>& polygons, const std::string& param);
+    void min(const std::vector<panchenko::Polygon>& polygons);
+    void max(const std::vector<panchenko::Polygon>& polygons);
+    void count(const std::vector<panchenko::Polygon>& polygons, const std::string& param);
+    void lessArea(const std::vector<panchenko::Polygon>& polygons, const panchenko::Polygon& polygon);
+    void same(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons);
 }
 
 #endif
