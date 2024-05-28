d80b78d94622418b23d65f1b8a08a82420f38df1
yahwee <163851516+yahwee@users.noreply.github.com>
2024-05-28 21:06:48 +0300

    changed functions

--- panchenko.maria/T3/commands.h
+++ panchenko.maria/T3/commands.h
@@ -7,15 +7,15 @@
 #include <vector>
 #include <cmath>
 #include <string>
-
 const std::string ERROR_OF_COMMAND_MSG = "<INVALID COMMAND>";
+
 namespace panchenko {
-    void area(const std::string& param);
-    void min();
-    void max();
-    void count(const std::string& param);
-    void lessArea(const panchenko::Polygon& polygon);
-    void same(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons);
+	void area(const std::vector<panchenko::Polygon>& polygons, const std::string& param);
+	void min(const std::vector<panchenko::Polygon>& polygons);
+	void max(const std::vector<panchenko::Polygon>& polygons);
+	void count(const std::vector<panchenko::Polygon>& polygons, const std::string& param);
+	void lessArea(const std::vector<panchenko::Polygon>& polygons, const panchenko::Polygon& polygon);
+	void same(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons);
 }
 
 #endif
