0c909b31a67167a884b98942a7c9eecc256931fe
yahwee <163851516+yahwee@users.noreply.github.com>
2024-05-29 22:27:01 +0300

    Update main.cpp delete test

--- panchenko.maria/T3/main.cpp
+++ panchenko.maria/T3/main.cpp
@@ -22,14 +22,11 @@ int main(int argc, char* argv[]) {
 
     std::vector<panchenko::Polygon> polygons;
     panchenko::Polygon polygon;
-    std::cout << "Starting to read file" << std::endl;
     while (!file.eof()) {
         file >> polygon;
         if (!file.fail()) {
-            std::cout << "Polygon read successfully" << std::endl;
             polygons.push_back(polygon);
         } else {
-            std::cout << "Read failed - skipping line" << std::endl;
             file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
     }
