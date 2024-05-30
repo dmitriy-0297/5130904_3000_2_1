d7dec467355293f36773e0d02e084f723c305998
brilnn <148279337+brilnn@users.noreply.github.com>
2024-05-30 16:15:08 +0300

    added empty polygons check

--- bril.nikolina/T3/Polygon.cpp
+++ bril.nikolina/T3/Polygon.cpp
@@ -158,7 +158,10 @@ void executeCommand(const std::string& command, std::vector<Polygon>& polygons) 
     else if (cmd == "COUNT") {
         std::string type;
         iss >> type;
-
+        if (polygons.empty()) {
+            std::cout << "<INVALID COMMAND>";
+            return;
+        }
         if (type == "EVEN") {
             auto isEven = [](const Polygon& p) { return p.points.size() % 2 == 0; };
             int count = std::count_if(polygons.begin(), polygons.end(), isEven);
